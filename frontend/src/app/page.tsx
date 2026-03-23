45e4101fab855df928a9efefdb84e090fb30182a
DouglasSalvagni <douglassalvagni@gmail.com>
2026-03-23 18:22:16 -0300

    ajustes e remove landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,5 @@
-import NavbarInspira from "@/components/inspira/NavbarInspira";
-import HeroInspira from "@/components/inspira/HeroInspira";
-import FeaturesInspira from "@/components/inspira/FeaturesInspira";
-import BenefitsInspira from "@/components/inspira/BenefitsInspira";
-import ManifestoInspira from "@/components/inspira/ManifestoInspira";
-import AppDownloadInspira from "@/components/inspira/AppDownloadInspira";
-import FooterInspira from "@/components/inspira/FooterInspira";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div className={`${quicksand.className} min-h-screen bg-brand-dark text-white selection:bg-brand-teal selection:text-brand-dark`}>
-      <NavbarInspira />
-      <main>
-        <HeroInspira />
-        <FeaturesInspira />
-        <BenefitsInspira />
-        <ManifestoInspira />
-        <AppDownloadInspira />
-      </main>
-      <FooterInspira />
-    </div>
-  );
+export default function RootPage() {
+  redirect("/auth/login");
 }
-import { Quicksand } from "next/font/google";
-
-const quicksand = Quicksand({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
